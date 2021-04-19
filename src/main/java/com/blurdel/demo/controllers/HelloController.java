acf514093abde03d9bf11702fa2071664eb3ace8
blurdel <blurdel@gmail.com>
2021-04-19 17:53:00 -0400

    small change in mapping

--- src/main/java/com/blurdel/demo/controllers/HelloController.java
+++ src/main/java/com/blurdel/demo/controllers/HelloController.java
@@ -5,12 +5,13 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Lazy
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
@@ -20,7 +21,7 @@ public class HelloController {
 		LOG.info("HelloController::");
 	}
 	
-	@GetMapping
+	@RequestMapping
 	public String getHello() {
 		return "CICD Demo: " + new Date().toString();
 	}

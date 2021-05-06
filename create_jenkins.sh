1980e3bd2f3f5445f7bdf1a9fc68e5dfc0e9ca28
blurdel <blurdel@gmail.com>
2021-05-06 15:50:36 -0400

    Update create_jenkins.sh
    Set Jenkins port to 8080

--- create_jenkins.sh
+++ create_jenkins.sh
@@ -6,11 +6,11 @@
 # After running this script once, you can start/stop Jenkins using:
 #   docker <start/stop> local_jenkins
 #
-# For the server, I chose to use port 8443, ie: localhost:8443
+# For the server, I chose to use port 8080, ie: localhost:8080
 #
 # TODO: First need to make the local volume (localVol) where Jenkins can persist data.
 #   sudo mkdir -p /opt/docker/jenkins/jenkins_home
-#   sudo chmod -R 777 /opt/docker/jenkins/jenkins_home
+#   sudo chmod -R 777 /opt/docker/jenkins
 # Docker will do it but with root permissions, which is not what you want.
 # The local volume can be found after the command line switch -v localVol:containerVol
-docker run --name local_jenkins -d -p 8443:8080 -p 50000:50000 -v /opt/docker/jenkins/jenkins_home:/var/jenkins_home  jenkins/jenkins:lts
+docker run --name local_jenkins -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins/jenkins_home:/var/jenkins_home  jenkins/jenkins:lts

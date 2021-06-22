1b6f1476ec5ba8c3b842c0e536bf3f8b370551e4
blurdel <blurdel@gmail.com>
2021-06-21 21:25:16 -0400

    Jenkins now runs in a container as root, added REST call for remote build

--- create_jenkins.sh
+++ create_jenkins.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# This script will create a Jenkins container image named 'local_jenkins' from
+# This script will create a Jenkins container image named 'jenkins' from
 # the official Jenkins LTS image.
 #
 # After running this script once, you can start/stop Jenkins using:
-#   docker <start/stop> local_jenkins
+#   docker <start/stop> jenkins
 #
-# For the server, I chose to use port 8080, ie: localhost:8080
+# For the server, I use port 8080, ie: localhost:8080
 #
 # TODO: First need to make the local volume (localVol) where Jenkins can persist data.
 #   sudo mkdir -p /opt/docker/jenkins/jenkins_home
 #   sudo chmod -R 777 /opt/docker/jenkins
 # Docker will do it but with root permissions, which is not what you want.
 # The local volume can be found after the command line switch -v localVol:containerVol
-docker run --name local_jenkins -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins/jenkins_home:/var/jenkins_home  jenkins/jenkins:lts
+docker run -d --name jenkins \
+-u root \
+-v /opt/docker/jenkins/jenkins_home:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $(which docker):/usr/bin/docker \
+-p 8080:8080 -p 50000:50000 \
+jenkins/jenkins:lts

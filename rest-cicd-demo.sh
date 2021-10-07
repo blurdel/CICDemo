69d513a79554191a6f168c7f4431245d27d104d9
blurdel <blurdel@gmail.com>
2021-10-07 10:41:19 -0400

    Update rest-cicd-demo.sh

--- rest-cicd-demo.sh
+++ rest-cicd-demo.sh
@@ -3,15 +3,15 @@
 me=$(basename $0)
 echo "me: ${me} $@"
 
-if [[ $# -ne 2 ]]; then
-  echo "Usage: ${me} <jobname> <branch>"
-  exit
-fi
+#if [[ $# -ne 2 ]]; then
+#  echo "Usage: ${me} <jobname> <branch>"
+#  exit
+#fi
 
-job=${1}
-branch=${2}
+#job=${1}
+#branch=${2}
 
-URL="http://localhost:8080/job/${job}/job/${branch}/buildWithParameters?param1=p1&version=1.1.0&unitTests=true"
+URL="http://localhost:8080/job/CICDemo/job/master/buildWithParameters?param1=p1&version=1.1.0&unitTests=true"
 echo "URL: ${URL}"
 
 ret=$(curl -X POST -vL --user auto:11472f530977d43e7f45ba5642d7fe7750  "${URL}")
